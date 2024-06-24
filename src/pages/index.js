f727d424eef525a055fdfae9c557261db308e75a
Raja <raja.rathinam@pcisage.com>
2024-06-24 12:30:01 +0530

    migration

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,12 @@ import supabase from "@/utils/db";
 const Home = ({ reviews }) => {
   console.log(reviews);
 
-  return <HomePage reviews={reviews} />;
+  return <HomePage reviews={reviews||[]} />;
 };
 
 export default Home;
 
-export async function getStaticProps() {
+export async function getServerSideProps () {
   const res = await supabase.from("reviews").select().limit(10);
 
   console.log(res);
